256cb5996ff2bead433debd5cea8d8e0fcef5329
AlanCechi1 <alancechi188@gmail.com>
2024-04-01 01:42:24 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Luxuosos from "./components/Luxuosos"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6  lg:flex justify-center items-center">
-      <section className="lg:flex max-w-[920px] ">
+      <div className="lg:flex max-w-[920px] ">
         <Sedans />
         <Suvs />
         <Luxuosos />
-      </section>
+      </div>
     </main>
   )
 }
